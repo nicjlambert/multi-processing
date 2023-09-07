6c38375a15449b0e52f53bec65e092ebad2fccb5
lambert <lambni85@outlook.com>
2023-09-07 11:17:20 +0000

    clean up downloads

--- multi_url_fetcher.py
+++ multi_url_fetcher.py
@@ -8,16 +8,14 @@ from multiprocessing import Pool
 import time
 import os
 
-def download(link):
-    os.system(f"aria2c {link}")
 
-#create an empty list using an empty pair of square brackets []
+def download(link):
+    os.system(f"wget {link} -x") 
+
 links = []
 
 for i in range(1,7):
     links.append(f"https://www.abs.gov.au/statistics/economy/business-indicators/business-indicators-australia/jun-2022/567600{i}.xlsx")
-
-#print(links)
 
 with Pool(7) as p:
     print(p.map(func=download, iterable=links))
